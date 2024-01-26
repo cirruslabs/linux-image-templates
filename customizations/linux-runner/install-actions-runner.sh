2ca1d025a26592134bc8d26e866b7b6afbb1ff2a
fedor <fedor.korotkov@gmail.com>
2024-01-26 13:16:49 -0500

    Fixed amd64 build

--- customizations/linux-runner/install-actions-runner.sh
+++ customizations/linux-runner/install-actions-runner.sh
@@ -10,7 +10,9 @@
 #
 set -euo pipefail
 
-DOWNLOAD_URL=$(curl -sS 'https://api.github.com/repos/actions/runner/releases/latest' | jq --raw-output '.assets[] | select(.name | test("actions-runner-linux-arm64-[0-9.]+.tar.gz")) | .browser_download_url')
+export RUNNER_ARCH=$(uname -m)
+
+DOWNLOAD_URL=$(curl -sS 'https://api.github.com/repos/actions/runner/releases/latest' | jq --raw-output --arg RUNNER_ARCH "$RUNNER_ARCH" '.assets[] | select(.name | test("actions-runner-linux-\($RUNNER_ARCH)-[0-9.]+.tar.gz")) | .browser_download_url')
 
 rm -rf actions-runner && mkdir actions-runner && cd actions-runner
 
